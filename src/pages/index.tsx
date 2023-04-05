cc93976c5df1537026f6d48beaed13c9a0b9c492
Mark Hall <mark.hall993@gmail.com>
2023-04-04 20:35:39 -0600

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
 
 
 const Home: NextPage = () => {
 
   const user = useUser();
+
+  const { data } = api.post.getAll.useQuery();
 
   return (
     <>
@@ -19,7 +22,9 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
       </main>
     </>
   );
